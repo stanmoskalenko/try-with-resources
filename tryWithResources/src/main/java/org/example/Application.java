f9ed81665bf7a1109a442dc4a6b6181698e81037
stanmoskalenko <st.moskalenko@gmail.com>
2023-10-10 21:34:05 +0300

    initial commit

---
+++ tryWithResources/src/main/java/org/example/Application.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Application {
+    public static void main(String[] args) throws SQLException {
+        // Соединение с базой данных тоже нужно отслеживать
+        try (var conn = DriverManager.getConnection("jdbc:h2:mem:hexlet_test")) {
+
+            var sql = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone VARCHAR(255))";
+            try (var statement = conn.createStatement()) {
+                statement.execute(sql);
+            }
+
+            var sql2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
+            try (var statement2 = conn.createStatement()) {
+                statement2.executeUpdate(sql2);
+            }
+
+            var sql3 = "SELECT * FROM users";
+            try (var statement3 = conn.createStatement()) {
+                var resultSet = statement3.executeQuery(sql3);
+                while (resultSet.next()) {
+                    System.out.println(resultSet.getString("username"));
+                    System.out.println(resultSet.getString("phone"));
+                }
+            }
+        }
+    }
+}
